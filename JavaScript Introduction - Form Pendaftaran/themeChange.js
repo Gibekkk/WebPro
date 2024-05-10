cf34ffaf07adb31a676a120f3085f1c2a931f48f
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-05-10 16:35:33 +0800

    theme to adjust by time for default

--- JavaScript Introduction - Form Pendaftaran/themeChange.js
+++ JavaScript Introduction - Form Pendaftaran/themeChange.js
@@ -14,9 +14,25 @@ function changeTheme() {
     }
 }
 
-// Fungsi untuk memeriksa tema saat ini dan mengatur ulang sesuai dengan tema tersebut
+
 function checkTheme(){
-    
+    // Mendapatkan string waktu lokal dan mengambil bagian waktu saja
+    var waktu = new Date().toLocaleString().split(", ")[1];
+    // Mengambil jam dari string waktu
+    var jam = waktu.split(":")[0];
+    // Mengambil setting AM atau PM dari string waktu
+    var timeSetting = waktu.split(" ")[1];
+    // Jika waktu menunjukkan PM, konversi jam ke format 24 jam
+    if(timeSetting == "PM") jam += 12;
+    // Jika jam menunjukkan 24, ubah menjadi 0 untuk representasi tengah malam
+    if(jam == 24) jam = 0;
+    // Jika jam antara 6 pagi dan 18 sore, atur tema ke light
+    if(jam >= 6 && jam < 18){
+        toLight()
+    } else{
+        // Jika tidak, atur tema ke dark
+        toDark()
+    }
 }
 
 // Fungsi untuk mengubah tema ke dark
@@ -47,4 +63,4 @@ function toLight(){
     themeButton.innerHTML = "Light Mode";
 }
 
-
+checkTheme()
